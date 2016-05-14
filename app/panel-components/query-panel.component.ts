530b6384a4fe22f9b27948316807523c7e07b880
Leonardo Priori <leonardopriori@gmail.com>
2016-05-14 18:42:48 -0300

    starting query panel

--- app/panel-components/query-panel.component.ts
+++ app/panel-components/query-panel.component.ts
@@ -1,15 +1,21 @@
 
 import { Component, Input } from '@angular/core'
 import {PanelModel} from "../services/panels.service";
+import {EditorComponent} from "../components/editor.component";
+import {GridComponent} from "../components/grid.component";
 
 export class QueryPanelModel implements PanelModel{
     type = "query";
     active;
-    title = "Nova Consulta"
+    title = "New Query"
+    code = ""
 }
 
 @Component({
-    template: 'query... {{model | json}}',
+    template: `
+        <editor code="model.code" style="height:300px;display:block;position:absolute;top:0;left:0;right:0"></editor>
+        <grid style="top:300px"></grid>` ,
+    directives: [EditorComponent,GridComponent],
     selector: 'query-panel'
 })
 export class QueryPanelComponent{
