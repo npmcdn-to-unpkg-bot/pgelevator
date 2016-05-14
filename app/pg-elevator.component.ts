d8688af1c18608577bac7ad554c92d1690d0b3e2
Leonardo Priori <leonardopriori@gmail.com>
2016-05-14 03:20:38 -0300

    editor component

--- app/pg-elevator.component.ts
+++ app/pg-elevator.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import {NavComponent} from './nav/nav.component';
 import {TabContentComponent} from './tab-content/tab-content.component';
 import {TabsComponent} from './tabs/tabs.component';
+import {EditorComponent} from './editor/editor.component';
 
 @Component({
   selector: 'pg-elevator',
-  directives: [NavComponent,TabContentComponent,TabsComponent],
+  directives: [NavComponent,TabContentComponent,TabsComponent,EditorComponent],
   template: `
-    <nav></nav><tabs></tabs><tab-content></tab-content>
+   <nav></nav><tabs></tabs><tab-content></tab-content>
   `
 })
 export class PgElevatorComponent { }
