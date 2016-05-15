ef11c20329959e2fa95b838260c353408746aa9f
Leonardo Priori <leonardopriori@gmail.com>
2016-05-14 21:08:12 -0300

    'typo'

--- app/modal-components/modal.component.ts
+++ app/modal-components/modal.component.ts
@@ -10,7 +10,7 @@ import { Component, ElementRef, OnInit, OnDestroy } from '@angular/core'
     `,
     styles: [`
         :host-context { content: ''; position: fixed; background: rgba(0,0,0,.5); left: 0; top: 0; bottom: 0;
-            right: 0; z-index: 5~ç; }
+            right: 0; z-index: 5; }
         :host-context .modal-content {position: absolute; left: 50px; top: 50px; background: white; padding: 10px; 
             box-shadow: 0 1px 10px rgba(0,0,0,.5); }
             
