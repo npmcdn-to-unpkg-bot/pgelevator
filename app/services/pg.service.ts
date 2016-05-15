0c9eb3e1a43cb9786774c717bb564133ae42293c
Anderson Vioto <anderson.vioto@gmail.com>
2016-05-15 19:57:01 -0300

    ..

--- app/services/pg.service.ts
+++ app/services/pg.service.ts
@@ -219,13 +219,13 @@ export var PgService = {
         return this.query(sql, schemaName)
     },
     listDataTypes(){
-        let sql=`SELECT DISTINCT pg_catalog.format_type(t.oid, NULL) types
+        let sql=`SELECT DISTINCT pg_catalog.format_type(t.oid, NULL) d_types
             FROM pg_catalog.pg_type t
                 LEFT JOIN pg_catalog.pg_namespace n ON n.oid = t.typnamespace
             WHERE (t.typrelid = 0 OR (SELECT c.relkind = 'c' FROM pg_catalog.pg_class c WHERE c.oid = t.typrelid))
             AND NOT EXISTS(SELECT 1 FROM pg_catalog.pg_type el WHERE el.oid = t.typelem AND el.typarray = t.oid)
             AND pg_catalog.pg_type_is_visible(t.oid)
-            ORDER BY 1, 2;`;
+            ORDER BY 1;`;
         return this.query(sql)
 
     },
