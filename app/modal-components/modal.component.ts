6a9595d8d515a2b50012545caedb296480a3ced0
Leonardo Priori <leonardopriori@gmail.com>
2016-05-14 21:12:23 -0300

    'modal...'

--- app/modal-components/modal.component.ts
+++ app/modal-components/modal.component.ts
@@ -1,26 +1,13 @@
-/**
- * Created by francisco on 14/05/16.
- */
 import { Component, ElementRef, OnInit, OnDestroy } from '@angular/core'
 
 @Component({
     selector: 'modal',
-    template: `
-        <div class="modal-content"><ng-content></ng-content></div>
-    `,
+    template: `<div class="modal-content"><ng-content></ng-content></div>`,
     styles: [`
         :host-context { content: ''; position: fixed; background: rgba(0,0,0,.5); left: 0; top: 0; bottom: 0;
             right: 0; z-index: 5; }
         :host-context .modal-content {position: absolute; left: 50px; top: 50px; background: white; padding: 10px; 
             box-shadow: 0 1px 10px rgba(0,0,0,.5); }
-            
     `]
 })
-export class ModalComponent implements OnInit{
-
-    constructor(private ref: ElementRef){}
-
-    ngOnInit() {
-        this.ref.nativeElement
-    }
-}
+export class ModalComponent {}
