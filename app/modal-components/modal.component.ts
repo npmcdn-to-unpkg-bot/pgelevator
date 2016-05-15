2c4229c67593c4d5105f061bc96a34c91173d29d
Leonardo Priori <leonardopriori@gmail.com>
2016-05-14 21:06:37 -0300

    modal component...

--- app/modal-components/modal.component.ts
+++ app/modal-components/modal.component.ts
@@ -6,44 +6,21 @@ import { Component, ElementRef, OnInit, OnDestroy } from '@angular/core'
 @Component({
     selector: 'modal',
     template: `
-        <div class="mask">
-            <div class="content" [style.width]="width">
-                <ng-content></ng-content>
-            </div>
-        </div>
+        <div class="modal-content"><ng-content></ng-content></div>
     `,
     styles: [`
-        .mask { 
-            background-color: #000; opacity: .6; position: absolute; top: 0; right: 0; left: 0; bottom: 0;
-            width: 100%; height: 100%; transition: all .2s;
-        }
-        .content { background-color: #f8f8f8; color: #404040; position: relative; margin: auto auto; 
+        :host-context { content: ''; position: fixed; background: rgba(0,0,0,.5); left: 0; top: 0; bottom: 0;
+            right: 0; z-index: 5~ç; }
+        :host-context .modal-content {position: absolute; left: 50px; top: 50px; background: white; padding: 10px; 
+            box-shadow: 0 1px 10px rgba(0,0,0,.5); }
             
-        }
     `]
 })
-export class ModalComponent implements OnInit, OnDestroy{
-    el:HTMLElement
-    height
-    width
+export class ModalComponent implements OnInit{
 
     constructor(private ref: ElementRef){}
 
-    ngOnInit():any {
-        console.log('init ...')
-        this.el = this.ref.nativeElement
-        let content:Element = this.el.getElementsByClassName('content')[0].firstElementChild as Element
-        console.log(content)
-        this.height = window.getComputedStyle(content,null).getPropertyValue("height");
-        this.width = window.getComputedStyle(content,null).getPropertyValue("width");
-
-        /*let mask: HTMLElement  = document.createElement('div')
-        mask.classList.add('mask')
-        this.el.parentElement.insertBefore(this.el)
-        mask.appendChild(this.el)*/
-    }
-
-    ngOnDestroy():any {
-        console.log('destroy...')
+    ngOnInit() {
+        this.ref.nativeElement
     }
 }
